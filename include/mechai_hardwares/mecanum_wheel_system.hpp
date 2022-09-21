4db490b75823a5304e17a4fdc93624d688ce7ca6
Jing Chen <abackcountryengineer@gmail.com>
2022-09-21 13:21:01 +0000

    ros2 control hardware system module setup

---
+++ include/mechai_hardwares/mecanum_wheel_system.hpp
@@ -0,0 +1,15 @@
+#ifndef MECANUM_WHEEL_SYSTEM__MECANUM_WHEEL_SYSTEM_HPP_
+#define MECANUM_WHEEL_SYSTEM__MECANUM_WHEEL_SYSTEM_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_lifecycle/state.hpp>
+
+namespace mecanum_wheel_system {
+  class MecanumWheelSystem {
+    public:
+      MecanumWheelSystem();
+    private:
+  };
+}
+
+#endif
