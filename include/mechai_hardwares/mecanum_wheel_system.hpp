16ae293487854220fbfc83d353a29c22e6eae5e6
Jing Chen <abackcountryengineer@gmail.com>
2022-09-23 13:19:18 +0000

    mecanum wheel hardware system header setup

--- include/mechai_hardwares/mecanum_wheel_system.hpp
+++ include/mechai_hardwares/mecanum_wheel_system.hpp
@@ -4,10 +4,27 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_lifecycle/state.hpp>
 
+#include <hardware_interface/system_interface.hpp>
+
 namespace mecanum_wheel_system {
-  class MecanumWheelSystem {
+  class MecanumWheelSystem : public hardware_interface::SystemInterface {
     public:
       MecanumWheelSystem();
+
+      CallbackReturn on_init(const hardware_interface::HardwareInfo & hardware_info) override;
+
+      CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state);
+
+      CallbackReturn on_cleanup(const rclcpp_lifecycle::State & previous_state);
+      
+      CallbackReturn on_shutdown(const rclcpp_lifecycle::State & previous_state);
+
+      CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state);
+
+      CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state);
+
+      CallbackReturn on_error(const rclcpp_lifecycle::State & previous_state);
+
     private:
   };
 }
